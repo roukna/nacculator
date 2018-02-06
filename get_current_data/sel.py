b064735240278413230590d64bd37116f0741deb
AjanthaRamineni <ajantha.5779@gmail.com>
2018-02-06 12:17:10 -0500

    Navigating to Upload a file with no erros

--- get_current_data/sel.py
+++ get_current_data/sel.py
@@ -8,7 +8,7 @@ def read_config(config_path):
     config.read(config_path)
     return config
 
-def upload_file(driver,filepath):
+def upload_file(driver,filepath,email):
     elem1 = driver.find_element_by_name("UPLOADR_X")
     driver.execute_script("(arguments[0]).click();", elem1)
     driver.find_element_by_name("FILEUP").send_keys(filepath)
@@ -17,6 +17,8 @@ def upload_file(driver,filepath):
         print "errors upload new file"
     else:
         print "file uploaded Succesfully"
+        driver.find_element_by_xpath("/html/body/table[2]/tbody/tr[2]/td[2]/input").click()
+        driver.find_element_by_name("EMAIL").send_keys(email)
     return
 
 def get_nacc_data(driver):
@@ -35,35 +37,38 @@ def replace_link(element, username, password):
 
 def main(argv):
     config = read_config("packet_config.ini")
-    driver = webdriver.Chrome()
+    try:
+        
+        driver = webdriver.Chrome()
+        options = argv
+        username = config.get('credentials','username')
+        password = config.get('credentials','password')
+        email = config.get('credentials', 'email')
+        filepath = config.get('uploadpath','path')
 
-    options = argv
-    username = config.get('credentials','username')
-    password = config.get('credentials','password')
-    filepath = config.get('uploadpath','path')
-    # Sign In credentials along with nacc url
-    str = "http://"+username+":"+password+"@www.alz.washington.edu/MEMBER/sitesub.htm"
-    driver.get(str)
-    # Get the 1Florida ADRC project url
-    nacc_project_elem = driver.find_element_by_xpath("/html/body/table[2]/tbody/tr[1]/td[2]/font/ul[1]/li[5]/a")
-    florida_adrc_link = replace_link(nacc_project_elem, username, password)
-    print florida_adrc_link
-    driver.get(florida_adrc_link)
-    # get the uds link
-    uds_project = driver.find_element_by_xpath("//*[@id='m_3subsystem_graphicC']/area[1]")
-    uds_link = replace_link(uds_project, username, password)
-    print uds_link
-    # Navigate to uds link
-    driver.get(uds_link)
+        # Sign In credentials along with nacc url
+        str = "http://"+username+":"+password+"@www.alz.washington.edu/MEMBER/sitesub.htm"
+        driver.get(str)
+        # Get the 1Florida ADRC project url
+        nacc_project_elem = driver.find_element_by_xpath("/html/body/table[2]/tbody/tr[1]/td[2]/font/ul[1]/li[5]/a")
+        florida_adrc_link = replace_link(nacc_project_elem, username, password)
+        driver.get(florida_adrc_link)
+        # get the uds link
+        uds_project = driver.find_element_by_xpath("//*[@id='m_3subsystem_graphicC']/area[1]")
+        uds_link = replace_link(uds_project, username, password)
+        # Navigate to uds link
+        driver.get(uds_link)
 
-    uds_nacc_results = driver.find_element_by_xpath("//*[@id='bodytable']/form/font/b/button").click()
-    if options == "upload":
-        upload_file(driver,filepath)
-    if options == "getdata":
-        get_nacc_data(driver)
+        uds_nacc_results = driver.find_element_by_xpath("//*[@id='bodytable']/form/font/b/button").click()
+        if options == "upload":
+            upload_file(driver,filepath,email)
+        if options == "getdata":
+            get_nacc_data(driver)
 
-    assert "No results found." not in driver.page_source
-    driver.close()
+        driver.close()
+
+    except Exception as e:
+        driver.close()
 
 if __name__ == '__main__':
     main(sys.argv[1])
